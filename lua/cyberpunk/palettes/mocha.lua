d331a558bb7c024dfa32391e19d88b1a689541d8
Mads Byriel <madshbyriel@gmail.com>
2026-03-04 10:03:02 +0100

    keyword update

--- lua/cyberpunk/palettes/mocha.lua
+++ lua/cyberpunk/palettes/mocha.lua
@@ -15,7 +15,7 @@ return {
 	green = "#00ff9f", -- strings
 
 	rosewater = "#d600ff", -- git titles, ???
-	mauve = "#55ead4", -- keywords
+	mauve = "#ea3061", -- keywords
 	red = "#d600ff", -- this keyword?
 	maroon = "#880425", -- parameter names
 	yellow = "#cccc00", -- class names, imports
